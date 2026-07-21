45e0b42c469ce3dfc74189e387eec7d89b113c90
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-07-21 22:48:09 +0530

    Connected to MongoDB atlas

--- src/index.js
+++ src/index.js
@@ -34,25 +34,25 @@ connect_DB ()
 
 
 
-// import express from 'express'
+import express from 'express'
 
-// const app = express()
+const app = express()
 
-// ;( async () => {
-//     try {
-//         const connectionInsatant = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-//         app.on('error', (err) => {
-//             console.error("ERROR", err);
-//             throw err
-//         })
+;( async () => {
+    try {
+        const connectionInsatant = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        app.on('error', (err) => {
+            console.error("ERROR", err);
+            throw err
+        })
 
-//         app.listen(process.env.PORT, () => {
-//             console.log('Server is running on port', process.env.PORT);
-//         })
-//     } 
-//     catch (err) {
-//         console.error("ERROR", err);
-//         throw err;
-//     }
+        app.listen(process.env.PORT, () => {
+            console.log('Server is running on port', process.env.PORT);
+        })
+    } 
+    catch (err) {
+        console.error("ERROR", err);
+        throw err;
+    }
 
-// }) //IIFE
+}) //IIFE
