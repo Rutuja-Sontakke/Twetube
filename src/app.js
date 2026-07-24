d360821eb4183cfa81a2b2206d70a219d9fde240
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-07-24 14:25:08 +0530

    All Setup config

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-const app = express;
+const app = express();
 
 app.use(cors ({
     origin: process.env.CORS_ORIGIN,
@@ -12,7 +12,15 @@ app.use(cors ({
 app.use(express.json({limit:"16kb"})) //Configuration 
 app.use(express.urlencoded({extended: true, limit: "16kb"})) //Configuration 
 app.use(express.static("public"))    //Configuration for images 
-app.use(cookiesParser())
+app.use(cookieParser())
 
 
-export {app}
+//routes import
+import userRouter from './routes/user.route.js'
+
+
+//routes declaration
+app.use("/api/v1/users", userRouter)
+
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv'
 import connect_DB from './db/index.js'
-
+import {app} from './app.js'
 
 dotenv.config({
     path: './.env'
@@ -10,13 +10,18 @@ dotenv.config({
 
 connect_DB ()
 .then(() => {
-    app.listen(process.env.PORT || 1000, () => {
+    const port = process.env.PORT ||1000;
+    app.listen(port, () => {
         console.log(` Server is running on PORT : ${process.env.PORT}`);
-    })
+    });
+    app.on("error", (error) => {
+        console.log("ERR: ", error);
+        throw error;
+    }); 
 })
 .catch((err) => {
     console.log("MongoDB connection failed !!", err)
-} ) 
+}) 
 
 
 
