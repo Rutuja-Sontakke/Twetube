3603cf34f73c6dd30106e3d890bb891bc9b915b5
Rutuja Sontakke <rutujasontakke50@gmail.com>
2026-07-24 13:30:49 +0530

    Cloudaniry and Multer setup for file uploading

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,5 +1,6 @@
 import multer from "multer"
 
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, ",/public/temp")
@@ -9,6 +10,9 @@ const storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
+
+
+
 
 export const upload = multer({
     storage,
